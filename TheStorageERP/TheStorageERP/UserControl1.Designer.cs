ea5a48f5a5d8b2702fc45344403c4f23131e7a26
AchiCatty <bulsago2001@google.com>
2020-12-15 08:47:56 +0900

    add Control

---
+++ TheStorageERP/TheStorageERP/UserControl1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace TheStorageERP
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.mapControl = new DevExpress.XtraMap.MapControl();
+            this.imageLayer1 = new DevExpress.XtraMap.ImageLayer();
+            this.bingMapDataProvider1 = new DevExpress.XtraMap.BingMapDataProvider();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.mapControl);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(666, 622);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // mapControl
+            // 
+            this.mapControl.Layers.Add(this.imageLayer1);
+            this.mapControl.Location = new System.Drawing.Point(12, 14);
+            this.mapControl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.mapControl.Name = "mapControl";
+            this.mapControl.Size = new System.Drawing.Size(642, 594);
+            this.mapControl.TabIndex = 4;
+            this.imageLayer1.DataProvider = this.bingMapDataProvider1;
+            this.bingMapDataProvider1.BingKey = "AubFnqHZplmeCYwVruQ5HelHfa6Xk92I0BvwGlraSadj2NiNuNQ3aAldhzn1apNb";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(666, 622);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.mapControl;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(646, 598);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.layoutControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(666, 622);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraMap.MapControl mapControl;
+        private DevExpress.XtraMap.ImageLayer imageLayer1;
+        private DevExpress.XtraMap.BingMapDataProvider bingMapDataProvider1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+    }
+}
---
+++ TheStorageERP/TheStorageERP/UserControl1.cs
@@ -0,0 +1,100 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraMap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheStorageERP
+{
+    public partial class UserControl1 : XtraUserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            mapControl.MapItemClick += OnPinClick;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            mapControl.CenterPoint = new GeoPoint(37.54164, 126.84035);
+            mapControl.ZoomLevel = 13;
+
+            VectorItemsLayer itemsLayer = new VectorItemsLayer();
+            mapControl.Layers.Add(itemsLayer);
+
+            MapItemStorage storage = new MapItemStorage();
+            MapItem[] capitals = GetCapitals();
+            storage.Items.AddRange(capitals);
+            itemsLayer.Data = storage;
+        }
+
+        private void OnPinClick(object sender, MapItemClickEventArgs e)
+        {
+            MessageBox.Show(((MapPushpin)e.Item).Text);
+        }
+
+
+        // 리스트 생성
+        MapItem[] GetCapitals()
+        {
+            return new MapItem[] {
+                new MapPushpin() { Text = "화곡역", Location = new GeoPoint(37.54164, 126.84035) },
+                new MapPushpin() { Text = "송정역", Location = new GeoPoint(37.56124, 126.81155) },
+                new MapPushpin() { Text = "영등포구청역", Location = new GeoPoint(37.52422, 126.89512) },
+                new MapPushpin() { Text = "합정역", Location = new GeoPoint(37.54904, 126.91336) },
+                new MapPushpin() { Text = "가양역", Location = new GeoPoint(37.56099, 126.85495) }
+            };
+        }
+
+        //없어도 가능
+        #region PinClicked event things for C# 3.0
+        public event EventHandler<PinClickedEventArgs> PinClicked;
+
+        protected virtual void OnPinClicked(PinClickedEventArgs e)
+        {
+            if (PinClicked != null)
+                PinClicked(this, e);
+        }
+
+        private PinClickedEventArgs OnPinClicked(string name)
+        {
+            PinClickedEventArgs args = new PinClickedEventArgs(name);
+            OnPinClicked(args);
+
+            return args;
+        }
+
+        private PinClickedEventArgs OnPinClickedForOut()
+        {
+            PinClickedEventArgs args = new PinClickedEventArgs();
+            OnPinClicked(args);
+
+            return args;
+        }
+
+        public class PinClickedEventArgs : EventArgs
+        {
+            public string Name { get; set; }
+
+            public PinClickedEventArgs()
+            {
+            }
+
+            public PinClickedEventArgs(string name)
+            {
+                Name = name;
+            }
+        }
+        #endregion
+
+
+    }
+}
